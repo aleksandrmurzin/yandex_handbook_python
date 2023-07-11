6344e7a85844a419e4945bc12654d5ac3805cfd7
Aleksandr Murzin <am@Aleksandrs-MacBook-Air.local>
2023-07-11 21:54:06 +0300

    complete

--- 2.2/T.py
+++ 2.2/T.py
@@ -1,9 +1,24 @@
-def main():
+def main(seq1, seq2, seq3):
     """
-    Entry point of the program.
+    Check if the word "зайка" is present in the given sequence.
+
+    Parameters:
+    seq (str): A string containing a sequence of words.
+
+    Prints:
+    "YES" if the word "зайка" is present in the sequence, otherwise "NO".
     """
-    # Write your code here
+    seqs = []
+    for seq in [seq1, seq2, seq3]:
+        if "зайка" in seq.split(" "):
+            seqs.append(seq)
+    min_seq = sorted(seqs)[0]
+    min_length = len(min_seq)
+    print(f"{min_seq} {min_length}")
 
 
 if __name__ == "__main__":
-    main()
+    given_seq1 = input()
+    given_seq2 = input()
+    given_seq3 = input()
+    main(seq1=given_seq1, seq2=given_seq2, seq3=given_seq3)
