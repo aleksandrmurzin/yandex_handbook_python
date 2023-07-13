9d5a57c4bd5088a3b088b43c5432ed0c028d9324
Aleksandr Murzin <am@Aleksandrs-MacBook-Air.local>
2023-07-13 23:52:51 +0300

    K-Q

--- 2.3/N.py
+++ 2.3/N.py
@@ -1,9 +1,15 @@
-def main():
+def main(number):
     """
     """
-    # Write your code
-    return None
+    if number == 1:
+        return print("NO")
+
+    for i in range(1, number + 1):
+        if number % i == 0 and i not in [1, number]:
+            return print("NO")
+    return print("YES")
 
 
 if __name__ == "__main__":
-    main()
+    number = int(input())
+    main(number=number)
--- 2.3/O.py
+++ 2.3/O.py
@@ -1,9 +1,25 @@
-def main():
+counter = 0
+
+
+def main(line):
     """
+    Increment the counter if the word "зайка" is present in the input line.
+
+    Args:
+        line (str): The input line to check.
+
+    Returns:
+        None
     """
-    # Write your code
+    global counter
+
+    if "зайка" in line.split(" "):
+        counter += 1
     return None
 
 
 if __name__ == "__main__":
-    main()
+    total_lines = int(input())
+    for i in range(total_lines):
+        main(line=input())
+    print(counter)
